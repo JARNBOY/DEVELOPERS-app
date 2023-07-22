b8a90bfb20029866b4d50e31022429a938ace418
Papon <papon.s@lawyerplusgroup.com>
2023-07-22 13:37:50 +0700

    Initial Commit

---
+++ DEVELOPERS/DEVELOPERSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEVELOPERSApp.swift
+//  DEVELOPERS
+//
+//  Created by Papon Supamongkonchai on 22/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DEVELOPERSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
